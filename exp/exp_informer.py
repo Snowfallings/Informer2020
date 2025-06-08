9dc357390126d16797f7927c624494dda33fc87b
Snowfallings <59189013+Snowfallings@users.noreply.github.com>
2025-06-08 21:57:23 +0800

    Update exp_informer.py

--- exp/exp_informer.py
+++ exp/exp_informer.py
@@ -72,6 +72,7 @@ class Exp_Informer(Exp_Basic):
             'Count':Dataset_Custom,
             'Diff_Count':Dataset_Custom,
             'features_Count':Dataset_Custom,
+            'features_Count_LN':Dataset_Custom,
         }
         Data = data_dict[self.args.data]
         timeenc = 0 if args.embed!='timeF' else 1
